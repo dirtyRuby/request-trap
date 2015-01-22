96618e07fac671245a9d2c2dc364f47073928bb6
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-01-22 13:46:00 +0200

    Initial routes configure.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'traps/show'
+  get '/' => 'traps#show'
+  get '/:trap_id/requests' => 'traps#show', as: 'trap'
+  get '/:trap_id/requests/:id' => 'requests#show', as: 'request'
 
-  get 'traps/capture'
-
-  get 'requests/show'
-
-  get 'request/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
