2433c2542ddc66180f5001e87118004cc8660b51
dirtyRuby <Dirty-Glamer@yandex.ru>
2015-02-13 21:08:58 +0200

    Migration fix.

--- db/migrate/20150122160018_add_dom_name_to_request.rb
+++
@@ -1,9 +0,0 @@
-class AddTrapIdToRequest < ActiveRecord::Migration
-  def up
-    add_column :requests, :dom_name, :string
-  end
-
-  def down
-    remove_column :requests, :dom_name
-  end
-end
--- db/migrate/20150128190511_remove_dom_name_from_requests.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDomNameFromRequests < ActiveRecord::Migration
-  def up
-    remove_column :requests, :dom_name
-  end
-
-  def down
-    add_column :requests, :dom_name, :string
-  end
-end
